ba8789babeeb7b9688315b8af14958360cb2ab70
DiyanLyubchev <diyan.lyubchev@gmail.com>
2024-03-16 19:28:08 +0200

    Initial commit

---
+++ BookReader/BookReader/Models/BookDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace BookReader.Models;
+
+public class BookDetails
+{
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public int Pages { get; set; }
+    public string Author { get; set; }
+    public byte[] Picture { get; set; }
+}
